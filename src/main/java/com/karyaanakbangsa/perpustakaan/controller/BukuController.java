e2178d942a7dd1dc12e302ac67275d4c5fc0b7a9
Hmlna <myhanif04@gmail.com>
2024-12-16 14:14:52 +0700

    fix merge

--- src/main/java/com/karyaanakbangsa/perpustakaan/controller/BukuController.java
+++ src/main/java/com/karyaanakbangsa/perpustakaan/controller/BukuController.java
@@ -6,7 +6,13 @@ import org.springframework.web.bind.annotation.*;
 import com.karyaanakbangsa.perpustakaan.models.Buku;
 import com.karyaanakbangsa.perpustakaan.models.BukuDto;
 import com.karyaanakbangsa.perpustakaan.repository.BukuRepository;
+<<<<<<< HEAD
 import jakarta.validation.Valid;
+=======
+
+import jakarta.validation.Valid;
+
+>>>>>>> f57e72c (build: create for buku)
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,5 +33,34 @@ public class BukuController {
         var bukus = bukuRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("bukus", bukus);
         return "bukus/index";
+    }
+
+    @GetMapping("/create")
+    public String createBuku(Model model) {
+        BukuDto bukuDto = new BukuDto();
+        model.addAttribute("bukuDto", bukuDto);
+
+        return "bukus/create";
+    }
+
+    @PostMapping("/create")
+    public String createBuku(
+            @Valid @ModelAttribute BukuDto bukuDto,
+            BindingResult result
+            ) {
+
+        if (result.hasErrors()) {
+            return "bukus/create";
+        }
+
+        Buku buku = new Buku();
+        buku.setJudul(bukuDto.getJudul());
+        buku.setPenerbit(bukuDto.getPenerbit());
+        buku.setPengarang(bukuDto.getPengarang());
+        buku.setTahunTerbit(bukuDto.getTahunTerbit());
+
+        bukuRepository.save(buku);
+
+        return "redirect:/bukus";
     }
 }
