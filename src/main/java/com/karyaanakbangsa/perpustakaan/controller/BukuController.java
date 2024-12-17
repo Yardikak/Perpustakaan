28753e4955e44748ea572f1a69764977835bd764
Yanwar Ardika C. <yanwarardikacahyani77@gmail.com>
2024-12-17 22:24:58 +0700

    commit for merge before pull use case add buku environment

--- src/main/java/com/karyaanakbangsa/perpustakaan/controller/BukuController.java
+++ src/main/java/com/karyaanakbangsa/perpustakaan/controller/BukuController.java
@@ -2,6 +2,8 @@ package com.karyaanakbangsa.perpustakaan.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import com.karyaanakbangsa.perpustakaan.models.Buku;
 import com.karyaanakbangsa.perpustakaan.repository.BukuRepository;
 import org.springframework.ui.Model;
 import org.springframework.data.domain.*;
@@ -20,5 +22,12 @@ public class BukuController {
         var bukus = bukuRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("bukus", bukus);
         return "bukus/index";
+    }
+
+    @GetMapping("/{id}")
+    public String getBukuById(@PathVariable Long id, Model model) {
+        Buku buku = bukuRepository.getById(id);
+        model.addAttribute("buku", buku);
+        return "bukus/detail";
     }
 }
--- src/main/java/com/karyaanakbangsa/perpustakaan/repository/BukuRepository.java
+++ src/main/java/com/karyaanakbangsa/perpustakaan/repository/BukuRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BukuRepository extends JpaRepository<Buku, Long> {
-    
+    Buku getById(Long id);
 }
